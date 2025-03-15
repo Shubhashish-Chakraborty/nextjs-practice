e44ea46791d99b70bc50420717dea83418b631c9
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-15 22:54:48 +0530

    again trying to fix

--- app/api/v1/auth/user/signup/route.ts
+++ app/api/v1/auth/user/signup/route.ts
@@ -1,12 +1,7 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
 export const GET = () => {
     return NextResponse.json({
         message: "SignUp Endpoint is ALright!!!!!"
     })
 }
-
-export const POST = async (req:NextRequest) => {
-
-    return NextResponse.json({message: "okay"})
-} 
