6a20a2d8ef73f1fe47d32d343ac544822e7a84be
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-16 14:05:51 +0530

    ignored ts errors

--- app/db/index.ts
+++ app/db/index.ts
@@ -4,12 +4,10 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
+//@ts-ignore
 const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
 
+//@ts-ignore
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
